0ff6d4745910819c9e5c371a69321fd553480ae2
Yarden Porat <yardenporat@gmail.com>
2020-04-26 10:20:14 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,51 @@
+//Scroll to view 
+
+$(document).ready(function() {
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .not('[href="#popup"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        document.getElementById("navi-toggle").checked=false;
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+});
+
+//Nav uncheck on click
+
+// const changedHash = () => {
+//   const id = window.location.hash.replace('#','');
+//   console.log(id)
+// };
+// // event listener
+// window.addEventListener('hashchange', changedHash);
